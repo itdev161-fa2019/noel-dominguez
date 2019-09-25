db15238eca3cd83684c30043357231934ac2dcdb
noel-dominguez <domingn2@gmatc.matc.edu>
2019-09-24 22:15:24 -0500

    added database connection

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 const app = express();
+
+connectDatabase();
 
 app.get('/', (req, res) =>
     res.send('http get reuest sent to root api endpoint')
     );
 
-    app.listen(3000, () => console.log('Express server running on port 3000'));
+app.listen(3000, () => console.log('Express server running on port 3000'));
