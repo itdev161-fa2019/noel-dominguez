39fcca0f48fa03519372fa78d151010bb90c0cdb
noel-dominguez <domingn2@gmatc.matc.edu>
2019-10-03 23:37:42 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -3,10 +3,29 @@ import connectDatabase from './config/db';
 
 const app = express();
 
+//connect db
 connectDatabase();
+
+//configure middleware
+app.use(express.json({ extended: false}));
+
+//API endpoints
+/**
+ * @route GET /
+ * @desc test end point
+ */
 
 app.get('/', (req, res) =>
     res.send('http get reuest sent to root api endpoint')
     );
+/**
+ * @route POST api/user
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+}
 
+//connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
