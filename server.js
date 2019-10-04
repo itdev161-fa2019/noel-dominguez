3309177f3d6fa49b12465054139b584ecea47792
noel-dominguez <domingn2@gmatc.matc.edu>
2019-10-04 00:36:34 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -30,13 +30,14 @@ app.post('/api/users', [
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ],
     (req, res) => {
-      const errors = validation(req);
+      const errors = validationResult(req);
       if (!errors.isEmpty()){
           return res.status(422).json({ errors: errors.array() });
       } else {
           return res.send(req.body);
       }
-    });
+    }
+    );
 
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
